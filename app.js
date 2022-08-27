85847a4f5a63064a774104e2840494f0249b5e81
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-27 14:59:49 -0500

    16x16 grid using grid

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 function gridAdd(insert, container, text) {
-    insert.classList.add('grid')
-    insert.textContent = text
-    container.appendChild(insert)
+	insert.classList.add('grid')
+	insert.textContent = text
+	container.appendChild(insert)
 }
 
 const gridContainer = document.querySelector('.gridContainer')
 // const gridDiv = document.createElement('div')
 
-for (i=1; i<=256; i++) {
-    const gridDiv = document.createElement('div')
-    gridAdd(gridDiv, gridContainer, i)
+for (i = 1; i <= 256; i++) {
+	const gridDiv = document.createElement('div')
+	gridAdd(gridDiv, gridContainer, i)
 }
 
 const grids = document.querySelectorAll('.grid')
 
 grids.forEach(square => {
-    square.addEventListener('hover', function(){
-        square.classList.add('hover')
-    })
+	square.addEventListener('mouseover', function () {
+		square.classList.add('hover')
+	})
 });
-    
