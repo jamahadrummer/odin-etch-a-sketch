0eb8b3180066268d3236460d928a8d1fbd5a14bb
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-10-01 13:06:50 -0500

    gridMath function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function gridAdd(insert, container, text) {
 	insert.classList.add('grid');
 	insert.textContent = text;
 	container.appendChild(insert);
-}
+};
 
 function gridClear(container) {
 	container.innerHTML = '';
@@ -17,14 +17,22 @@ function gridBuild(size) {
 	grids.forEach(square => {
 		square.addEventListener('mouseover', function () {
 			square.classList.add('hover');
-		})
-	})
+		});
+	});
+};
+
+function gridMath(input) {
+	if (input > 0) {
+		let output = input * input
+		return output
+	} else { return NaN }
 }
 
+
 function checkForm() {
-	let value = document.getElementById('gridSize').value;
-	return value 
-}
+	let value = document.querySelector('#gridSize').value;
+	return value
+};
 
 const gridContainer = document.querySelector('.gridContainer');
 const reset = document.querySelector('#reset');
@@ -33,9 +41,10 @@ const enter = document.querySelector('#enter');
 
 enter.addEventListener('click', function () {
 	let input = checkForm()
+	let totalGrid = gridMath(input)
 	gridClear(gridContainer);
-	gridBuild(input);
-})
+	gridBuild(totalGrid);
+});
 
 reset.addEventListener('click', function () {
 	const grids = document.querySelectorAll('.grid')
