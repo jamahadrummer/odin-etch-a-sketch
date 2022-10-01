d578e4e46a6c62a1274bb84da4c9974badb836f6
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-10-01 11:40:49 -0500

    semi colons and gridclear function

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 function gridAdd(insert, container, text) {
-	insert.classList.add('grid')
-	insert.textContent = text
-	container.appendChild(insert)
+	insert.classList.add('grid');
+	insert.textContent = text;
+	container.appendChild(insert);
 }
+
+function gridClear(container){
+	container.innerHTML = '';
+};
 
 function gridBuild(size) {
 	for (i = 1; i <= size; i++) {
-		const gridDiv = document.createElement('div')
-		gridAdd(gridDiv, gridContainer, i)
+		const gridDiv = document.createElement('div');
+		gridAdd(gridDiv, gridContainer, i);
 	}
-	const grids = document.querySelectorAll('.grid')
+	const grids = document.querySelectorAll('.grid');
 	grids.forEach(square => {
 		square.addEventListener('mouseover', function () {
-			square.classList.add('hover')
+			square.classList.add('hover');
 		})
 	})
 }
 
-const gridContainer = document.querySelector('.gridContainer')
-const reset = document.querySelector('#reset')
-const enter = document.querySelector('#enter')
+const gridContainer = document.querySelector('.gridContainer');
+const reset = document.querySelector('#reset');
+const enter = document.querySelector('#enter');
 
 enter.addEventListener('click', function () {
-	gridBuild(256)
+	gridClear(gridContainer);
+	gridBuild(256);
 })
 
 reset.addEventListener('click', function () {
 	const grids = document.querySelectorAll('.grid')
 	grids.forEach(square => {
-		square.classList.remove('hover')
-	})
-})
+		square.classList.remove('hover');
+	});
+});
